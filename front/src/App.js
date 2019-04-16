afa70be4e62025b28a97e59a82a0954c2954b1fa
Clement Cornut <ccornut@vente-privee.com>
2019-04-16 12:13:11 +0200

    Implement toast

--- front/src/App.js
+++ front/src/App.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'; 
 import Admin from './Components/Admin';
 import Receiver from './Components/Receiver';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/admin" component={Admin} />
-          <Route path="/receiver" component={Receiver} />
-        </Switch>
-      </BrowserRouter>
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/admin" component={Admin} />
+            <Route path="/receiver" component={Receiver} />
+          </Switch>
+        </BrowserRouter>
+        <ToastContainer position="bottom-right"/>
+      </div>
     );
   }
 }
