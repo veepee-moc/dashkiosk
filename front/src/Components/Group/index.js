0ca49b68da1af5be5c235b91cf52082a9117a24b
Romain LEROY <roleroy@vente-privee.com>
2019-04-19 15:02:27 +0200

    State to props  for group

--- front/src/Components/Group/index.js
+++ front/src/Components/Group/index.js
@@ -8,7 +8,6 @@ import Display from '../Display';
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import { SortableHandle } from 'react-sortable-hoc';
 import Droppable from '../DragAndDrop/Droppable';
-import Draggable from '../DragAndDrop/Draggable';
 import ModalDashboard from '../Modals/dashboard';
 
 const DragHandle = SortableHandle(() =>
@@ -21,41 +20,16 @@ class Group extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: 0,
-            title: 'Unassigned',
-            description: 'Newly created group',
-            displays: {},
             layoutSize: 3
         };
         this.Rest = new Rest(this);
-        this.updateGroupInfo = this.updateGroupInfo.bind(this);
         this.renderDisplays = this.renderDisplays.bind(this);
         this.closeModal = this.closeModal.bind(this);
         this.openModal = this.openModal.bind(this);
     }
 
-    updateGroupInfo() {
-        if (this.props.group) {
-            this.setState({
-                id: this.props.group.id,
-                title: this.props.group.name,
-                description: this.props.group.description,
-                displays: this.props.group.displays
-            });
-        }
-    }
-
-    componentDidMount() {
-        this.updateGroupInfo();
-    }
-
-    componentDidUpdate(prevProps) {
-        if (this.props.group !== prevProps.group)
-            this.updateGroupInfo();
-    }
-
     renderDisplays() {
-        return Object.values(this.state.displays).map((display) =>
+        return Object.values(this.props.group.displays).map((display) =>
             <CSSTransition key={display.id} classNames="fade" timeout={{ enter: 500, exit: 300 }}>
                 <li className="list-layout-item" key={display.id} style={{ width: 100 / this.state.layoutSize + '%' }}>
                     <Display display={display} />
@@ -75,47 +49,47 @@ class Group extends Component {
     render() {
         return (
             <Droppable types={["Display"]}>
-            <div className="card">
-                <div className="card-header pt-1 pr-2 pb-0 pl-2">
-                    <div className="float-right">
-                        <Swap className="float-left" control={!(Object.keys(this.state.displays).length === 0)}>
-                            <button className="btn btn-noframe-dark p-1 pl-2 pr-2" onClick={ this.Rest.deleteGroup }>
-                                <IoMdTrash />
-                            </button>
-                            <div hidden={ Object.values(this.state.displays).find((obj) => obj.connected) === undefined }>
-                                <button className="btn btn-noframe-dark p-1 pl-2 pr-2" onClick={ this.Rest.reloadGroupDisplays }>
-                                    <IoMdRefresh />
+                <div className="card">
+                    <div className="card-header pt-1 pr-2 pb-0 pl-2">
+                        <div className="float-right">
+                            <Swap className="float-left" control={!(Object.keys(this.props.group.displays).length === 0)}>
+                                <button className="btn btn-noframe-dark p-1 pl-2 pr-2" onClick={ this.Rest.deleteGroup }>
+                                    <IoMdTrash />
                                 </button>
-                                <button className="btn btn-noframe-dark p-1 pl-2 pr-2" onClick={ this.Rest.toggleOSD }>
-                                    <IoMdLocate />
-                                </button>
-                            </div>
-                        </Swap>
-                        <DragHandle className="float-right" />
+                                <div hidden={ Object.values(this.props.group.displays).find((obj) => obj.connected) === undefined }>
+                                    <button className="btn btn-noframe-dark p-1 pl-2 pr-2" onClick={ this.Rest.reloadGroupDisplays }>
+                                        <IoMdRefresh />
+                                    </button>
+                                    <button className="btn btn-noframe-dark p-1 pl-2 pr-2" onClick={ this.Rest.toggleOSD }>
+                                        <IoMdLocate />
+                                    </button>
+                                </div>
+                            </Swap>
+                            <DragHandle className="float-right" />
+                        </div>
+                        <EditableText className="card-title mb-0" text={ this.props.group.name }
+                            onSubmit={ this.Rest.updateGroupName } />
+                        <EditableText className="card-subtitle text-muted m-0 mb-1" text={ this.props.group.description }
+                            onSubmit={ this.Rest.updateGroupDescription } />
                     </div>
-                    <EditableText className="card-title mb-0" text={ this.state.title }
-                        onSubmit={ this.Rest.updateGroupName } />
-                    <EditableText className="card-subtitle text-muted m-0 mb-1" text={ this.state.description }
-                        onSubmit={ this.Rest.updateGroupDescription } />
+                    <div className="card-body pt-2 pb-2">
+                        <ul className="list-layout">
+                            <TransitionGroup>
+                                { this.renderDisplays() }
+                            </TransitionGroup>
+                        </ul>
+                    </div>
+                    <div className="btn-group btn-group-sm">
+                        <button type="button" className="btn btn-light w-50 border-right rounded-0" onClick={ this.openModal }>
+                            Add a new dashboard
+                        </button>
+                        <button type="button" className="btn btn-light w-50 border-left rounded-0" onClick={ this.Rest.preview }>
+                            Preview
+                        </button>
+                    </div>
+                    <ModalDashboard show={this.state.showModal} rest={this.Rest}
+                        group={{ name: this.props.group.name, id: this.props.group.id }} onHide={this.closeModal} />
                 </div>
-                <div className="card-body pt-2 pb-2">
-                    <ul className="list-layout">
-                        <TransitionGroup>
-                            { this.renderDisplays() }
-                        </TransitionGroup>
-                    </ul>
-                </div>
-                <div className="btn-group btn-group-sm">
-                    <button type="button" className="btn btn-light w-50 border-right rounded-0" onClick={ this.openModal }>
-                        Add a new dashboard
-                    </button>
-                    <button type="button" className="btn btn-light w-50 border-left rounded-0" onClick={ this.Rest.preview }>
-                        Preview
-                    </button>
-                </div>
-                <ModalDashboard show={this.state.showModal} rest={this.Rest}
-                    group={{ name: this.state.title, id: this.state.id }} onHide={this.closeModal} />
-            </div>
             </Droppable>
         );
     }
