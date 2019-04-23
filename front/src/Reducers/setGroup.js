bebfb05f3b034276ac48fdecc73d23a70068ac11
Romain LEROY <roleroy@vente-privee.com>
2019-04-23 16:51:11 +0200

    Fix SetGroup Reducer function

--- front/src/Reducers/setGroup.js
+++ front/src/Reducers/setGroup.js
@@ -1,6 +1,10 @@
 function setGroup(state, group) {
     const groups = [...state.admin.groups];
-    groups[group.rank] = group;
+    for (var i = 0; i < groups.length; i++) {
+        if (groups[i].id === group.id)
+            groups.splice(i, 1);
+    }
+    groups.splice(group.rank, 0, group);
     return Object.assign({}, state, {
         admin: Object.assign({}, state.admin, {
             groups: groups
