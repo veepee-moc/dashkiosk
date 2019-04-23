42a843d057a96caee87c458650e245cc78bd5f21
Clement Cornut <ccornut@vente-privee.com>
2019-04-23 16:51:59 +0200

    Dashboard text, description and color when active

--- front/src/Components/Dashboard/index.js
+++ front/src/Components/Dashboard/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { IoMdCreate } from 'react-icons/io';
+import { connect } from 'react-redux';
 import './Dashboard.css';
 
 class Dashboard extends Component {
@@ -11,22 +12,22 @@ class Dashboard extends Component {
 
     render() {
         return (
-            <div className={`list-group-item p-0 ${ this.props.isActive ? "active light-blue" : "" }`}>
-                <a className={`ml-1 name ${ this.props.isActive ? "text-white" : "text-dark" }`} href={ this.props.href } target="_blank">{ this.props.href }</a>
-                <button className="btn btn-noframe-dark p-1 pl-2 pr-2 float-right">
+            <li className={`list-group-item p-0 ${ this.props.dashboard.active ? "active light-blue" : "" }`}>
+                <a className={`ml-1 name ${ this.props.dashboard.active ? "text-white" : "text-dark" }`} href={ this.props.dashboard.url } target="_blank">
+                    { this.props.dashboard.description ? this.props.dashboard.description : this.props.dashboard.url }
+                </a>
+                <button className={`btn ${ this.props.dashboard.active ? "btn-noframe-light" : "btn-noframe-dark"} p-1 pl-2 pr-2 float-right`}>
                     <IoMdCreate />
                 </button>
-            </div>
+            </li>
         );
     }
 };
 
-Dashboard.propTypes = {
-
+function mapStateToProps(state, ownProps) {
+    return {
+        dashboard: state.admin.groups[ownProps.groupIndex].dashboards[ownProps.dashboardKey]
+    };
 }
 
-export default withRouter(Dashboard);
-
-/*<ul className="list-group list-group-flush">
-                        <Dashboard href="http://test.com"></Dashboard>
-                    </ul>*/
+export default connect(mapStateToProps)(Dashboard);
--- front/src/Components/Group/GroupBody/index.js
+++ front/src/Components/Group/GroupBody/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Display from '../../Display';
+import Dashboard from '../../Dashboard'
 
 class GroupBody extends Component {
     constructor(props) {
@@ -8,6 +9,12 @@ class GroupBody extends Component {
         this.state = {
             layoutSize: 3
         };
+    }
+
+    renderDashboard() {
+        return this.props.dashboards.map((key) =>
+            <Dashboard groupIndex={ this.props.groupIndex } dashboardKey={ key } />
+        );
     }
 
     renderDisplays() {
@@ -20,10 +27,17 @@ class GroupBody extends Component {
 
     render() {
         return (
-            <div className="card-body pt-2 pb-2">
-                <ul className="list-layout">
-                    { this.renderDisplays() }
-                </ul>
+            <div>
+                <div className="card-body pt-2 pb-2">
+                    <ul className="list-layout">
+                        { this.renderDisplays() }
+                    </ul>
+                </div>
+                <div>
+                    <ul className="list-group list-group-flush">
+                        { this.renderDashboard() }
+                    </ul>
+                </div>
             </div>
         );        
     }
@@ -32,7 +46,8 @@ class GroupBody extends Component {
 function mapStateToProps(state, ownProps) {
     const group = state.admin.groups[ownProps.groupIndex];
     return {
-        displays: group ? Object.keys(group.displays) : null
+        displays: group ? Object.keys(group.displays) : null,
+        dashboards: group ? Object.keys(group.dashboards) : null
     };
 }
 
