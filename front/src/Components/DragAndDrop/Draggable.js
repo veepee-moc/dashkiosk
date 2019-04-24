ff8793c43cc71b00e402a84e193fba0c57cf5781
Romain LEROY <roleroy@vente-privee.com>
2019-04-24 11:45:22 +0200

    Fix draggable

--- front/src/Components/DragAndDrop/Draggable.js
+++ front/src/Components/DragAndDrop/Draggable.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Types, action } from '../../Actions';
+import Store from '../../Store';
 
 class Draggable extends Component {
     constructor(props) {
@@ -8,13 +10,14 @@ class Draggable extends Component {
 
     handleDragStart(event) {
         event.dataTransfer.setData(this.props.type, this.props.draggableId);
+        Store.dispatch(action(Types.SetDragAndDrop, { type: this.props.type, object: this.props.children }));
     }
 
     render() {
         return (
-            <div draggable="true" onDragStart={ this.handleDragStart }>
+            <span draggable="true" onDragStart={ this.handleDragStart }>
                 { this.props.children }
-            </div>
+            </span>
         );
     }
 };
