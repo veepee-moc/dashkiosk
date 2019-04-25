a1e8d8ca32cdd86bf828f51be9591ddf0f8ad37c
Clement Cornut <ccornut@vente-privee.com>
2019-04-25 17:54:36 +0200

    Editable dashboard

--- front/src/Components/Dashboard/index.js
+++ front/src/Components/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { IoMdCreate, IoMdTimer, IoMdResize } from 'react-icons/io';
+import { IoMdCreate, IoMdTimer, IoMdResize, IoMdSync } from 'react-icons/io';
 import { connect } from 'react-redux';
 import './Dashboard.css';
 import EditableText from '../EditableText';
@@ -17,18 +17,37 @@ class Dashboard extends Component {
                     <a className={`col color-transition ml-1 name ${ this.props.dashboard.active ? "text-white" : "text-dark" }`} href={ this.props.dashboard.url } target="_blank" rel="noopener noreferrer">
                         { this.props.dashboard.description ? this.props.dashboard.description : this.props.dashboard.url }
                     </a>
-                    <span className="col-md-auto px-0">
+                    <span className="col-md-auto px-0" hidden={ this.props.dashboard.viewport === null ? true : false }>
                         <IoMdResize/>
                     </span>
-                    <span className="col-md-auto color-transition px-1">
-                        <span className="ml-1">
-                            <EditableText showButton={ false } text={ this.props.dashboard.url } onSubmit={ () => console.log("YO") } />
+                    <span className="col-md-auto color-transition pl-1 pr-0" hidden={ this.props.dashboard.viewport === null ? true : false }>
+                        <span>
+                            <EditableText showButton={ false } text={ this.props.dashboard.viewport } onSubmit={ () => console.log("YO") } />
                         </span>
                     </span>
-                    <span className="col-md-auto color-transition px-1">
-                        <IoMdTimer/>
-                        <span className="ml-1">2s</span>
+                    <span className="col-md-auto color-transition px-1" hidden={ this.props.dashboard.timeout === null ? true : false }>
+                        <IoMdSync/>
                     </span>
+                    <span className="col-md-auto color-transition pl-1 pr-0" hidden={ this.props.dashboard.timeout === null ? true : false }>
+                        <span>
+                            <EditableText showButton={ false } text={ this.props.dashboard.timeout } onSubmit={ () => console.log("YO") } />
+                        </span>
+                    </span>
+                    <span className="col-md-auto color-transition pl-0 pr-1" hidden={ this.props.dashboard.timeout === null ? true : false }>
+                        s
+                    </span>
+                    <span className="col-md-auto color-transition px-1" hidden={ this.props.dashboard.delay === null ? true : false }>
+                        <IoMdTimer/>
+                    </span>
+                    <span className="col-md-auto color-transition pl-1 pr-0" hidden={ this.props.dashboard.delay === null ? true : false }>
+                        <span>
+                            <EditableText showButton={ false } text={ this.props.dashboard.delay } onSubmit={ () => console.log("YO") } />
+                        </span>
+                    </span>
+                    <span className="col-md-auto color-transition pl-0 pr-1" hidden={ this.props.dashboard.delay === null ? true : false }>
+                        s
+                    </span>
+
                     <button className={`btn ${ this.props.dashboard.active ? "btn-noframe-light" : "btn-noframe-dark"} py-1 pl-2 pr-2 color-transition col-md-auto`}>
                         <IoMdCreate />
                     </button>
