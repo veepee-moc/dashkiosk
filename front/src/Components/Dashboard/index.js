14aa9b2da3aac678ade5d3af7fc5e4e0f4cdd168
Clement Cornut <ccornut@vente-privee.com>
2019-04-25 17:06:48 +0200

    Fix active dashboard

--- front/src/Components/Dashboard/index.js
+++ front/src/Components/Dashboard/index.js
@@ -12,28 +12,26 @@ class Dashboard extends Component {
 
     render() {
         return (
-            <li className={`bg-color-transition list-group-item p-0 ${ this.props.dashboard.active ? "active light-blue" : "" }`}>
-                <span className="container float-right">
-                    <span className="row align-items-center">
-                        <a className={`col color-transition ml-1 name ${ this.props.dashboard.active ? "text-white" : "text-dark" }`} href={ this.props.dashboard.url } target="_blank">
-                            { this.props.dashboard.description ? this.props.dashboard.description : this.props.dashboard.url }
-                        </a>
-                        <span className="col-md-auto px-0">
-                            <IoMdResize/>
-                        </span>
-                        <span className="col-md-auto color-transition px-1">
-                            <span className="ml-1">
-                                <EditableText showButton={ false } text={ this.props.dashboard.url } onSubmit={ () => console.log("YO") } />
-                            </span>
-                        </span>
-                        <span className="col-md-auto color-transition px-1">
-                            <IoMdTimer/>
-                            <span className="ml-1">2s</span>
-                        </span>
-                        <button className={`btn ${ this.props.dashboard.active ? "btn-noframe-light" : "btn-noframe-dark"} py-1 pl-2 pr-2 color-transition col-md-auto`}>
-                            <IoMdCreate />
-                        </button>
+            <li className={`container float-right bg-color-transition list-group-item py-0 pl-1 ${ this.props.dashboard.active ? "active light-blue" : "" }`}>
+                <span className="row align-items-center">
+                    <a className={`col color-transition ml-1 name ${ this.props.dashboard.active ? "text-white" : "text-dark" }`} href={ this.props.dashboard.url } target="_blank" rel="noopener noreferrer">
+                        { this.props.dashboard.description ? this.props.dashboard.description : this.props.dashboard.url }
+                    </a>
+                    <span className="col-md-auto px-0">
+                        <IoMdResize/>
                     </span>
+                    <span className="col-md-auto color-transition px-1">
+                        <span className="ml-1">
+                            <EditableText showButton={ false } text={ this.props.dashboard.url } onSubmit={ () => console.log("YO") } />
+                        </span>
+                    </span>
+                    <span className="col-md-auto color-transition px-1">
+                        <IoMdTimer/>
+                        <span className="ml-1">2s</span>
+                    </span>
+                    <button className={`btn ${ this.props.dashboard.active ? "btn-noframe-light" : "btn-noframe-dark"} py-1 pl-2 pr-2 color-transition col-md-auto`}>
+                        <IoMdCreate />
+                    </button>
                 </span>
             </li>
         );
