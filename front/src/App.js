f579df7c3150973e02a077ba0ca18a217cead826
Romain LEROY <roleroy@vente-privee.com>
2019-05-09 10:44:20 +0200

    Temporary fix to use MultiDashboards in dev environnement

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'; 
 import Admin from './Components/Admin';
 import Receiver from './Components/Receiver';
+import FromServer from './Components/FromServer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import './App.css';
@@ -14,6 +15,7 @@ class App extends Component {
           <Switch>
             <Route exact path={["/", "/receiver"]} component={Receiver} />
             <Route exact path="/admin" component={Admin} />
+            <Route path="/*" component={FromServer} />
           </Switch>
         </BrowserRouter>
         <ToastContainer position="bottom-right"/>
