6db5db71f36f42e918e2222b83f52b7d84c8f896
Clement Cornut <ccornut@vente-privee.com>
2019-05-13 11:01:38 +0200

    Remove old front

--- app/scripts/controllers/admin.js
+++
@@ -1,6 +0,0 @@
-angular.module('dashkiosk.controllers', []);
-
-require('./dashboard');
-require('./display');
-require('./group');
-require('./groups');
--- app/scripts/services/admin.js
+++
@@ -1,5 +0,0 @@
-angular.module('dashkiosk.services', []);
-
-require('./alerts');
-require('./groups');
-require('./loading.js');
