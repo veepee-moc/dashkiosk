196254ffeaeffd46234e255c3abb012b90217fdf
Nina <ndelcroix@vente-privee.com>
2019-05-14 16:09:19 +0200

    use cover or contain from settings configuration

--- front/src/Components/Receiver/DashboardImage.js
+++ front/src/Components/Receiver/DashboardImage.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import config from '../../config';
+import { connect } from 'react-redux';
 import './Receiver.css';
 
-export default function DashboardImage(props) {
+function DashboardImage(props) {
   let style = JSON.parse(JSON.stringify(props.style));
   style.backgroundImage = `url(${props.src})`;
-  style.backgroundSize = (props.viewport) ? style.backgroundSize : 'contain'; // config -> cover or contain
+  style.backgroundSize = (props.viewport) 
+    ? style.backgroundSize 
+    : (props.uploaded_images_format && props.uploaded_images_format === 'contain'
+      ? 'contain'
+      : 'cover');
   style.backgroundPosition = 'center';
   style.backgroundRepeat = 'no-repeat';
   style.transition = 'background-image 1s, opacity 1s';
@@ -13,3 +17,11 @@ export default function DashboardImage(props) {
     <div style={style} />
   );
 }
+
+function mapStateToProps(state) {
+	return ({
+    uploaded_images_format: state.settings.uploaded_images_format
+	});
+}
+
+export default connect(mapStateToProps)(DashboardImage);
