7770fc2340f4bf2e453ce9f2f8c9239b5cef1819
Romain LEROY <roleroy@vente-privee.com>
2019-05-14 10:31:56 +0200

    Fix some CSS issues

--- front/src/Components/SideMenu/GroupTagsMenu/index.js
+++ front/src/Components/SideMenu/GroupTagsMenu/index.js
@@ -22,6 +22,7 @@ class GroupTagsMenu extends Component {
     handleSubmit = (event) => {
         event.preventDefault();
         this.props.Rest.addNewTag(this.state.inputValue);
+        this.overlay.hide();
     }
     
     handleValueChanged = (event) => {
@@ -40,9 +41,9 @@ class GroupTagsMenu extends Component {
                 <form onSubmit={this.handleSubmit}>
                     <div className="input-group">
                         <input type="text" className="form-control form-control-sm"
-                            value={this.state.inputValue} onChange={this.handleValueChanged} />
+                            value={this.state.inputValue} onChange={this.handleValueChanged} autoFocus />
                         <div className="input-group-append">
-                            <button className="btn btn-primary btn-sm" onClick={this.handleSubmit}>
+                            <button className="btn btn-primary btn-sm">
                                 <IoMdCheckmark />
                             </button>
                         </div>
@@ -56,7 +57,7 @@ class GroupTagsMenu extends Component {
                 <div className="m-1">
                     { this.renderGroupTags() }
                 </div>
-                <OverlayTrigger trigger="click" rootClose placement="right" overlay={popover}>
+                <OverlayTrigger ref={(elem) => this.overlay = elem} trigger="click" rootClose placement="right" overlay={popover}>
                     <button className="btn btn-noframe-dark btn-sm d-block w-100 rounded-0 text-center">
                         <IoMdAdd style={{ fontSize: '20px' }} />
                     </button>
