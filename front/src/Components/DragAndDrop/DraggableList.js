f02725a4c190b501fc477304716e4c679eaf6389
Romain LEROY <roleroy@vente-privee.com>
2019-05-28 15:42:43 +0200

    Reorder dashboard by drag and drop

---
+++ front/src/Components/DragAndDrop/DraggableList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
+
+const DraggableList = ({ droppableId, children }) => {
+    return (
+        <Droppable droppableId={`${droppableId}`}>
+            {(provided) => (
+                <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}>
+                    {React.Children.map(children, (child, index) => (
+                        <Draggable key={index} draggableId={`${droppableId}-${index}`} index={index}>
+                            {(provided) => (
+                                <div
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                    ref={provided.innerRef}>
+                                    {child}
+                                </div>
+                            )}
+                        </Draggable>
+                    ))}
+                    {provided.placeholder}
+                </div>
+            )}
+        </Droppable>
+    );
+};
+
+export default DraggableList;
