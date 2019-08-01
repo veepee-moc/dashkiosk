c9be0c6aed241edebce331e85ade1655771728ef
Romain LEROY <roleroy@vente-privee.com>
2019-08-01 10:46:34 +0200

    Made a deep merge for SetConfigState action

--- back/src/Redux/Reducers/Config/Actions/SetConfigState.js
+++ back/src/Redux/Reducers/Config/Actions/SetConfigState.js
@@ -1,8 +1,20 @@
 const { Types } = require('../../../Actions');
 
+function assign(dst, src) {
+    for (const key in src) {
+        if (typeof(src[key]) === 'object' && dst.hasOwnProperty(key))
+            assign(dst[key], src[key]);
+        else
+            dst[key] = src[key]
+    }
+}
+
 module.exports = {
     type: Types.SetConfigState,
     do(state, config) {
-        return Object.assign({}, state, config);
+        const Config = Object.assign({}, state);
+        assign(Config, config);
+        console.log(Config);
+        return Object.assign({}, state, Config);
     }
 };
